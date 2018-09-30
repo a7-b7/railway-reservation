04195449ffa914eeb31f53d461e3b604818ad811
abhishekjha-wedmegood <abhishek_jha@wedmegood.com>
2018-09-30 09:01:37 +0530

    Made the changes for generating a PNR

---
+++ booktkt.php
@@ -0,0 +1,115 @@
+<?php 
+session_start();
+	if(empty($_SESSION['user_info'])){
+		echo "<script type='text/javascript'>alert('Please login before proceeding further!');</script>";
+	}
+$conn = mysqli_connect("localhost","root","","railway");
+if(!$conn){  
+	echo "<script type='text/javascript'>alert('Database failed');</script>";
+  	die('Could not connect: '.mysqli_connect_error());  
+}
+if (isset($_POST['submit']))
+{
+$trains=$_POST['trains'];
+$sql = "SELECT t_no FROM trains WHERE t_name = '$trains'";
+$result = mysqli_query($conn, $sql);
+$row = mysqli_fetch_assoc($result);
+
+
+$passengerquery = "SELECT id, p_lname, t_no FROM passengers WHERE t_no='$row[t_no]';";
+$passengerresult = mysqli_query($conn, $passengerquery);
+$passengerrow = mysqli_fetch_assoc($passengerresult);
+if(strlen($passengerrow[p_lname]) > 3)
+	$pnr_generated = $passengerrow[id].substr($passengerrow[p_lname], 0, 3).$passengerrow[t_no];
+else
+	$pnr_generated = $passengerrow[id].$passengerrow[p_lname].$passengerrow[t_no];
+
+
+$email=$_SESSION['user_info'];
+$query="UPDATE passengers SET t_no='$row[t_no]' WHERE email='$email';";
+	if(mysqli_query($conn, $query))
+{  
+	$pnr = "INSERT INTO tickets (t_status, PNR) VALUES ('CONFIRM', '$pnr_generated');";
+	if(mysqli_query($conn, $pnr))
+		$message = "Ticket booked successfully and PNR is generated";
+	else
+		$message = "Ticket booked successfully but couldnt generate PNR";
+}
+	else {
+		$message="Transaction failed";
+	}
+	echo "<script type='text/javascript'>alert('$message');</script>";
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Book a ticket</title>
+	<LINK REL="STYLESHEET" HREF="STYLE.CSS">
+	<style type="text/css">
+		#booktkt	{
+			margin:auto;
+			margin-top: 50px;
+			width: 40%;
+			height: 60%;
+			padding: auto;
+			padding-top: 50px;
+			padding-left: 50px;
+			background-color: rgba(0,0,0,0.3);
+			border-radius: 25px;
+		}
+		html { 
+		  background: url(img/bg7.jpg) no-repeat center center fixed; 
+		  -webkit-background-size: cover;
+		  -moz-background-size: cover;
+		  -o-background-size: cover;
+		  background-size: cover;
+		}
+		#journeytext	{
+			color: white;
+			font-size: 28px;
+			font-family:"Comic Sans MS", cursive, sans-serif;
+		}
+		#trains	{
+			margin-left: 90px;
+			font-size: 15px;
+		}
+		#submit	{
+			margin-left: 150px;
+			margin-bottom: 40px;
+			margin-top: 30px
+		}
+	</style>
+	<script type="text/javascript">
+		function validate()	{
+			var trains=document.getElementById("trains");
+			if(trains.selectedIndex==0)
+			{
+				alert("Please select your train");
+				trains.focus();
+				return false;		
+			}
+		}
+	</script>
+</head>
+<body>
+	<?php
+		include ('header.php');
+	?>
+	<div id="booktkt">
+	<h1 align="center" id="journeytext">Choose your journey</h1><br/><br/>
+	<form method="post" name="journeyform" onsubmit="return validate()">
+		<select id="trains" name="trains" required>
+			<option selected disabled>-------------------Select trains here----------------------</option>
+			<option value="rajdhani" >Rajdhani Express - Mumbai Central to Delhi</option>
+			<option value="duronto" >Duronto Express - Mumbai Central to Ernakulum</option>
+			<option value="geetanjali">Geetanjali Express - CST to Kolkata</option>
+			<option value="garibrath" >Garib Rath - Udaipur to Jammu Tawi</option>
+			<option value="mysoreexp" >Mysore Express - Talguppa to Mysore Jn</option>
+		</select>
+		<br/><br/>
+		<input type="submit" name="submit" id="submit" class="button" />
+	</form>
+	</div>
+	</body>
+	</html>
